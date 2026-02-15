93f0557b4a0e5ec86a5ab1636f2065cbce9b2201
JoJo <jonahthwaite@gmail.com>
2026-02-15 13:13:37 +0000

    initial backend routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,43 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors({
+  origin: ["http://localhost:5500", "http://localhost:8080"]
+}));
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.json({
+    msg: "This is the register/login page."
+  });
+});
+
+app.get("/home", (req, res) => {
+  res.json({
+    msg: "This is the home page."
+  })
+});
+
+app.get("/register", (req, res) => {
+  res.json({
+    msg: "This is the register form page."
+  })
+});
+
+app.get("/login", (req, res) => {
+  res.json({
+    msg: "This is the login form page."
+  })
+});
+
+app.get("/password", (req, res) => {
+  res.json({
+    msg: "This is the password form page."
+  })
 });
 
 app.listen(PORT, () => {
--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -6,6 +6,19 @@ import "./styles/global.css";
 import { HeaderComponent } from "./components/header/HeaderComponent";
 
 const root = document.getElementById("root")!;
+
+fetch("http://localhost:3000")
+  .then((res) => {
+    if (!res.status) {
+      throw new Error(`Server error: ${res.status} ${res.statusText}`);
+    }
+    return res.json();
+  })
+  .then((data) => {
+    root.innerText = data.msg;
+  })
+  .catch((err) => console.error(err));
+
 const header = new HeaderComponent();
 
 const [cols, rows] = DIFFICULTIES.EASY;
